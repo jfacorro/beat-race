9d716585320e33017d1571fdc1fabddf9deca043
juan.facorro <juan.facorro@gmail.com>
2012-01-07 02:10:17 -0300

    Got MP3 files playing with FMOD.

--- jni/main.c
+++ jni/main.c
@@ -21,13 +21,7 @@
 FMOD_SYSTEM  *gSystem        = 0;
 FMOD_SOUND	 *gSound         = 0;
 FMOD_CHANNEL *gChannel       = 0;
-FMOD_DSP     *gDSPLowpass    = 0;
-FMOD_DSP     *gDSPHighpass   = 0;
-FMOD_DSP     *gDSPEcho       = 0;
-FMOD_DSP     *gDSPFlange     = 0;
-FMOD_DSP     *gDSPDistortion = 0;
-FMOD_DSP     *gDSPChorus     = 0;
-FMOD_DSP     *gDSPParameq    = 0;
+
 
 void CHECK_RESULT(FMOD_RESULT result)
 {
@@ -38,7 +32,7 @@ void CHECK_RESULT(FMOD_RESULT result)
 	}
 }
 
-void Java_org_fmod_effects_Example_cBegin(JNIEnv *env, jobject thiz)
+void Java_com_facorro_beatrace_SongPlayerActivity_cBegin(JNIEnv *env, jobject thiz, jstring jfilename)
 {
 	FMOD_RESULT result = FMOD_OK;
 
@@ -47,33 +41,19 @@ void Java_org_fmod_effects_Example_cBegin(JNIEnv *env, jobject thiz)
 
 	result = FMOD_System_Init(gSystem, 32, FMOD_INIT_NORMAL, 0);
 	CHECK_RESULT(result);
-
-	result = FMOD_System_CreateSound(gSystem, "/sdcard/fmod/drumloop.wav", FMOD_SOFTWARE | FMOD_LOOP_NORMAL, 0, &gSound);
+	
+	const jbyte *filename;
+	filename = (*env)->GetStringUTFChars(env, jfilename, NULL);
+	
+	FMOD_MODE mode = FMOD_HARDWARE | FMOD_CREATESTREAM | FMOD_OPENONLY | FMOD_2D;
+	result = FMOD_System_CreateSound(gSystem, filename, mode, 0, &gSound);
 	CHECK_RESULT(result);
 
 	result = FMOD_System_PlaySound(gSystem, FMOD_CHANNEL_FREE, gSound, 0, &gChannel);
 	CHECK_RESULT(result);
-
-    /*
-        Create some effects to play with.
-    */
-    result = FMOD_System_CreateDSPByType(gSystem, FMOD_DSP_TYPE_LOWPASS, &gDSPLowpass);
-    CHECK_RESULT(result);
-    result = FMOD_System_CreateDSPByType(gSystem, FMOD_DSP_TYPE_HIGHPASS, &gDSPHighpass);
-    CHECK_RESULT(result);
-    result = FMOD_System_CreateDSPByType(gSystem, FMOD_DSP_TYPE_ECHO, &gDSPEcho);
-    CHECK_RESULT(result);
-    result = FMOD_System_CreateDSPByType(gSystem, FMOD_DSP_TYPE_FLANGE, &gDSPFlange);
-    CHECK_RESULT(result);
-    result = FMOD_System_CreateDSPByType(gSystem, FMOD_DSP_TYPE_DISTORTION, &gDSPDistortion);
-    CHECK_RESULT(result);
-    result = FMOD_System_CreateDSPByType(gSystem, FMOD_DSP_TYPE_CHORUS, &gDSPChorus);
-    CHECK_RESULT(result);
-    result = FMOD_System_CreateDSPByType(gSystem, FMOD_DSP_TYPE_PARAMEQ, &gDSPParameq);
-    CHECK_RESULT(result);
 }
 
-void Java_org_fmod_effects_Example_cUpdate(JNIEnv *env, jobject thiz)
+void Java_com_facorro_beatrace_SongPlayerActivity_cUpdate(JNIEnv *env, jobject thiz)
 {
 	FMOD_RESULT	result = FMOD_OK;
 
@@ -81,24 +61,9 @@ void Java_org_fmod_effects_Example_cUpdate(JNIEnv *env, jobject thiz)
 	CHECK_RESULT(result);
 }
 
-void Java_org_fmod_effects_Example_cEnd(JNIEnv *env, jobject thiz)
+void Java_com_facorro_beatrace_SongPlayerActivity_cEnd(JNIEnv *env, jobject thiz)
 {
 	FMOD_RESULT result = FMOD_OK;
-
-	result = FMOD_DSP_Release(gDSPLowpass);
-	CHECK_RESULT(result);
-	result = FMOD_DSP_Release(gDSPHighpass);
-	CHECK_RESULT(result);
-	result = FMOD_DSP_Release(gDSPEcho);
-	CHECK_RESULT(result);
-	result = FMOD_DSP_Release(gDSPFlange);
-	CHECK_RESULT(result);
-	result = FMOD_DSP_Release(gDSPDistortion);
-	CHECK_RESULT(result);
-	result = FMOD_DSP_Release(gDSPChorus);
-	CHECK_RESULT(result);
-	result = FMOD_DSP_Release(gDSPParameq);
-	CHECK_RESULT(result);
 
 	result = FMOD_Sound_Release(gSound);
 	CHECK_RESULT(result);
@@ -107,7 +72,7 @@ void Java_org_fmod_effects_Example_cEnd(JNIEnv *env, jobject thiz)
 	CHECK_RESULT(result);
 }
 
-void Java_org_fmod_effects_Example_cPause(JNIEnv *env, jobject thiz)
+void Java_com_facorro_beatrace_SongPlayerActivity_cPause(JNIEnv *env, jobject thiz)
 {
 	FMOD_RESULT result = FMOD_OK;
 	FMOD_BOOL paused = 0;
@@ -119,159 +84,7 @@ void Java_org_fmod_effects_Example_cPause(JNIEnv *env, jobject thiz)
 	CHECK_RESULT(result);
 }
 
-void Java_org_fmod_effects_Example_cLowpassToggle(JNIEnv *env, jobject thiz)
-{
-	FMOD_RESULT result = FMOD_OK;
-	FMOD_BOOL active = 0;
-
-    result = FMOD_DSP_GetActive(gDSPLowpass, &active);
-    CHECK_RESULT(result);
-
-    if (active)
-    {
-        result = FMOD_DSP_Remove(gDSPLowpass);
-        CHECK_RESULT(result);
-    }
-    else
-    {
-        result = FMOD_System_AddDSP(gSystem, gDSPLowpass, 0);
-        CHECK_RESULT(result);
-    }
-}
-
-void Java_org_fmod_effects_Example_cHighpassToggle(JNIEnv *env, jobject thiz)
-{
-	FMOD_RESULT result = FMOD_OK;
-	FMOD_BOOL active = 0;
-
-    result = FMOD_DSP_GetActive(gDSPHighpass, &active);
-    CHECK_RESULT(result);
-
-    if (active)
-    {
-        result = FMOD_DSP_Remove(gDSPHighpass);
-        CHECK_RESULT(result);
-    }
-    else
-    {
-        result = FMOD_System_AddDSP(gSystem, gDSPHighpass, 0);
-        CHECK_RESULT(result);
-    }
-}
-
-void Java_org_fmod_effects_Example_cEchoToggle(JNIEnv *env, jobject thiz)
-{
-	FMOD_RESULT result = FMOD_OK;
-	FMOD_BOOL active = 0;
-
-    result = FMOD_DSP_GetActive(gDSPEcho, &active);
-    CHECK_RESULT(result);
-
-    if (active)
-    {
-        result = FMOD_DSP_Remove(gDSPEcho);
-        CHECK_RESULT(result);
-    }
-    else
-    {
-        result = FMOD_System_AddDSP(gSystem, gDSPEcho, 0);
-        CHECK_RESULT(result);
-
-        result = FMOD_DSP_SetParameter(gDSPEcho, FMOD_DSP_ECHO_DELAY, 50.0f);
-        CHECK_RESULT(result);
-    }
-}
-
-void Java_org_fmod_effects_Example_cFlangeToggle(JNIEnv *env, jobject thiz)
-{
-	FMOD_RESULT result = FMOD_OK;
-	FMOD_BOOL active = 0;
-
-    result = FMOD_DSP_GetActive(gDSPFlange, &active);
-    CHECK_RESULT(result);
-
-    if (active)
-    {
-        result = FMOD_DSP_Remove(gDSPFlange);
-        CHECK_RESULT(result);
-    }
-    else
-    {
-        result = FMOD_System_AddDSP(gSystem, gDSPFlange, 0);
-        CHECK_RESULT(result);
-    }
-}
-
-void Java_org_fmod_effects_Example_cDistortionToggle(JNIEnv *env, jobject thiz)
-{
-	FMOD_RESULT result = FMOD_OK;
-	FMOD_BOOL active = 0;
-
-    result = FMOD_DSP_GetActive(gDSPDistortion, &active);
-    CHECK_RESULT(result);
-
-    if (active)
-    {
-        result = FMOD_DSP_Remove(gDSPDistortion);
-        CHECK_RESULT(result);
-    }
-    else
-    {
-        result = FMOD_System_AddDSP(gSystem, gDSPDistortion, 0);
-        CHECK_RESULT(result);
-
-        result = FMOD_DSP_SetParameter(gDSPDistortion, FMOD_DSP_DISTORTION_LEVEL, 0.8f);
-        CHECK_RESULT(result);
-    }
-}
-
-void Java_org_fmod_effects_Example_cChorusToggle(JNIEnv *env, jobject thiz)
-{
-	FMOD_RESULT result = FMOD_OK;
-	FMOD_BOOL active = 0;
-
-    result = FMOD_DSP_GetActive(gDSPChorus, &active);
-    CHECK_RESULT(result);
-
-    if (active)
-    {
-        result = FMOD_DSP_Remove(gDSPChorus);
-        CHECK_RESULT(result);
-    }
-    else
-    {
-        result = FMOD_System_AddDSP(gSystem, gDSPChorus, 0);
-        CHECK_RESULT(result);
-    }
-}
-
-void Java_org_fmod_effects_Example_cParameqToggle(JNIEnv *env, jobject thiz)
-{
-	FMOD_RESULT result = FMOD_OK;
-	FMOD_BOOL active = 0;
-
-    result = FMOD_DSP_GetActive(gDSPParameq, &active);
-    CHECK_RESULT(result);
-
-    if (active)
-    {
-        result = FMOD_DSP_Remove(gDSPParameq);
-        CHECK_RESULT(result);
-    }
-    else
-    {
-        result = FMOD_System_AddDSP(gSystem, gDSPParameq, 0);
-        CHECK_RESULT(result);
-
-		result = FMOD_DSP_SetParameter(gDSPParameq, FMOD_DSP_PARAMEQ_CENTER, 5000.0f);
-		CHECK_RESULT(result);
-
-		result = FMOD_DSP_SetParameter(gDSPParameq, FMOD_DSP_PARAMEQ_GAIN, 0.0f);
-		CHECK_RESULT(result);
-    }
-}
-
-jboolean Java_org_fmod_effects_Example_cGetPaused(JNIEnv *env, jobject thiz)
+jboolean Java_com_facorro_beatrace_SongPlayerActivity_cGetPaused(JNIEnv *env, jobject thiz)
 {
 	FMOD_RESULT result = FMOD_OK;
 	FMOD_BOOL paused = 0;
--- src/com/facorro/beatrace/SongPlayerActivity.java
+++ src/com/facorro/beatrace/SongPlayerActivity.java
@@ -1,11 +1,16 @@
 package com.facorro.beatrace;
 
+import org.fmod.FMODAudioDevice;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class SongPlayerActivity extends Activity {
+	
+	private FMODAudioDevice mFMODAudioDevice = new FMODAudioDevice();
+	private String filename;
 	
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,40 @@ public class SongPlayerActivity extends Activity {
         
         //SurfaceView view = (SurfaceView) this.findViewById(R.id.surfaceView);
         
-        String filename = intent.getStringExtra("filename");
-        text.setText(filename);
+        this.filename = intent.getStringExtra("filename");
+        this.filename = this.filename.replace("/mnt/", "/");
+        text.setText(this.filename);
 	}
+	
+    @Override
+    public void onStart()
+    {
+    	super.onStart();
+    	
+    	mFMODAudioDevice.start();
+    	
+    	cBegin(this.filename);
+    }
+    
+    @Override
+    public void onStop()
+    {
+    	cEnd();
+    	
+    	mFMODAudioDevice.stop();
+    	
+    	super.onStop();
+    }
+	
+    static 
+    {
+    	System.loadLibrary("fmodex");
+        System.loadLibrary("main");
+    }
+    
+	public native void cBegin(String filename);
+	public native void cUpdate();
+	public native void cEnd();
+	public native void cPause();
+	public native boolean cGetPaused();
 }
